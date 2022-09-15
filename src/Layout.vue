3269134f74b72e0fffa1e72c64ec1f76ddd052c8
Natalia <>
2022-09-15 18:00:05 +0200

    initial commit

---
+++ src/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="layout-app">
+    <h1>Vue App</h1>
+    {{ env }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    env () {
+      return process.env
+    }
+  }
+};
+</script>
